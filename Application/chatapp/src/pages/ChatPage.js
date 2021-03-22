26d944bda1818b1f46001d8be48d3f61491c64bb
tobiashjalmarsson <tobhja@student.chalmers.se>
2021-03-22 21:58:44 +0100

    (feat) Updating Store on form submit
    - Adding submitted messages in the chat to the Redux Store on submit.

--- Application/chatapp/src/pages/ChatPage.js
+++ Application/chatapp/src/pages/ChatPage.js
@@ -3,12 +3,22 @@ import { connect } from 'react-redux';
 import { addMsg } from '../redux/store';
 
 const ChatPage = (props) => {
+    const handleSubmit = (e) => {
+        const msg = e.target.elements.msg_box.value;
+        e.preventDefault();
+        console.log(msg);
+        props.dispatch(addMsg(msg));
+    }
     return (
         <div>
             <h1>ChatPage</h1>
             {props.messages.map((message) => {
                 return <p>{message}</p>
             })}
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="message" name="msg_box"></input>
+                <button>Send</button>
+            </form>
         </div>
     );
 }
@@ -21,9 +31,5 @@ const mapStateToProps = (state) => {
 }
 
 // Map our dispatch to the props of the component
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addMsg: () => dispatch(addMsg())
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(ChatPage);
+
+export default connect(mapStateToProps)(ChatPage);
